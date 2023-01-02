b11b6846435333895162eb79d4352c9af7fdbc04
William L. Thomson Jr <wlt@o-sinc.com>
2023-01-02 09:05:58 -0500

    check_linode.sh: Updated for Linode API v4 use tokens instead api key
    Initial support for the various statuses returned

--- check_linode.sh
+++ check_linode.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
-# Copyright 2015 Obsidian-Studios, Inc.
+# Copyright 2015-2023 Obsidian-Studios, Inc.
 # Author William L. Thomson Jr.
-# 	 wlt@o-sinc.com#
-
+# 	 wlt@o-sinc.com
+#
 # Distributed under the terms of The MIT License (MIT)
-
+#
 # check_linode.sh checks the status of a Linode using the Linode API.
-# Using a Linode API key and Linode ID as parameters and returning a
+# Using a Linode API token and Linode ID as parameters and returning a
 # Nagios plugin status code with message.
 
-VERSION="Version 0.1"
+VERSION="Version 0.2"
 
 print_help() {
-	echo "Usage: $0 -k <api_key> -l <linode_id>"
+	echo "Usage: $0 -t <token> -l <linode_id>"
 }
 
 print_version() {
@@ -21,7 +21,7 @@ print_version() {
 }
 
 if [[ -z ${1} ]]; then
-	display_help
+	print_help
 	exit 3
 fi
 
@@ -29,12 +29,12 @@ fi
 while :
 do
 	case "$1" in
-		-k | --api-key)
+		-t | --token)
 			if [[ -z ${2} ]]; then
-				echo "Linode API Key not specified aborting"
+				echo "Linode API token not specified aborting"
 				exit 3
 			fi
-			API_KEY="${2}"
+			TOKEN="${2}"
 			shift 2
 			;;
 		-l | --linode-id)
@@ -68,8 +68,8 @@ do
 done
 
 # Check required variables
-if [[ -z ${API_KEY} ]]; then
-	echo "Linode API Key not specified aborting"
+if [[ -z ${TOKEN} ]]; then
+	echo "Linode API token not specified aborting"
 	exit 3
 fi
 
@@ -78,7 +78,9 @@ if [[ -z ${LINODE_ID} ]]; then
 	exit 3
 fi
 
-RESPONSE=$(curl -s "https://api.linode.com/?api_key=${API_KEY}&api_action=linode.list&linodeid=${LINODE_ID}")
+RESPONSE=$(curl -s -H "Authorization: Bearer ${TOKEN}" \
+		"https://api.linode.com/v4/linode/instances/${LINODE_ID}" \
+	)
 
 # Check response
 if [[ -z ${RESPONSE} ]]; then
@@ -86,7 +88,7 @@ if [[ -z ${RESPONSE} ]]; then
 	exit 2
 fi
 
-STATUS="$(echo -e "${RESPONSE}" | sed 's/.*"STATUS":\([0-9]*\),.*/\1/')"
+STATUS="$(echo -e "${RESPONSE}" | sed 's/.* "status": "\([^"]*\).*/\1/')"
 
 # Check status
 if [[ -z ${STATUS} ]]; then
@@ -96,19 +98,19 @@ fi
 
 # Handle status
 case ${STATUS} in
-	-1) echo "WARNING - Linode Being Created"
+	provisioning|restoring) echo "WARNING - Linode Being Created"
 		exit 1
 		;;
-	0) echo "WARNING - Brand New Linode"
+	booting|rebooting) echo "WARNING - Linode Booting/Rebooting"
 		exit 1
 		;;
-	1) echo "OK - Linode Running"
+	running) echo "OK - Linode Running"
 		exit 0
 		;;
-	2) echo "CRITICAL - Linode Powered Off"
+	offline|stopped) echo "CRITICAL - Linode Powered Off"
 		exit 2
 		;;
-	*) echo "Unknown"
+	*) echo "Unknown status ${STATUS}"
 		exit 3
 		;;
 esac
